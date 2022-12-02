be51b5352f702910199961466e5a889524e296d3
Marc Granier <mgranier@comcast.net>
2022-12-02 18:06:35 -0500

    Order Entry:

---
+++ backend/routes/product.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const router = express.Router()
+
+const {
+	getProducts,
+	getAdminProducts,
+	newProduct,
+	getSingleProduct,
+	updateProduct,
+	deleteProduct,
+	createProductReview,
+	getProductReviews,
+	deleteReview
+} = require("../controllers/productController")
+
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth")
+
+router.route("/products").get(isAuthenticatedUser, getProducts)
+router.route("/product/:id").get(getSingleProduct)
+
+router
+	.route("/admin/product/new")
+	.post(isAuthenticatedUser, authorizeRoles("admin"), newProduct)
+
+router
+	.route("/admin/product/:id")
+	.put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+	.delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct)
+
+module.exports = router
